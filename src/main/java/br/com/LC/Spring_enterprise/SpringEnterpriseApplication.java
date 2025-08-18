b57c5b505b082ca99ced0251b23fe0a2ad45f877
LucasCrippa <lucas_crippa@hotmail.com>
2025-08-18 17:24:00 -0300

    Project create

---
+++ src/main/java/br/com/LC/Spring_enterprise/SpringEnterpriseApplication.java
@@ -0,0 +1,13 @@
+package br.com.LC.Spring_enterprise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEnterpriseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEnterpriseApplication.class, args);
+	}
+
+}
