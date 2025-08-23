cdc350acdc4d16b11c6107337a8819ac730ab9f9
LucasCrippa <lucas_crippa@hotmail.com>
2025-08-23 11:32:28 -0300

    Class SecurityConfiguration

--- src/main/java/br/com/LC/Spring_enterprise/Infra/SecurityConfiguration.java
+++ src/main/java/br/com/LC/Spring_enterprise/Infra/SecurityConfiguration.java
@@ -1,4 +1,21 @@
 package br.com.LC.Spring_enterprise.Infra;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfiguration {
+
+    @Bean
+    public UserDetailsService registerUsuaryData(){
+        UserDetails usuaryOne = User.builder().username("lucascrippa@teste.com.br").password("{noop} Mortadela").build();
+        return  new InMemoryUserDetailsManager(usuaryOne);
+    }
+
 }
